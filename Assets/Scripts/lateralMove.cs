e971b7aa36a33a095cd78ba4382f604c52ee33b9
Ebuus <eburtchbuus@mail.pima.edu>
2016-02-11 20:01:10 -0700

    First Commit Nub.

---
+++ Assets/Scripts/lateralMove.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lateralMove : MonoBehaviour {
+    public float jumpForce;
+    public float speed;
+    private Rigidbody2D latMovement;
+    private void Awake()
+    {
+        latMovement = GetComponent<Rigidbody2D>(); 
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        float move = Input.GetAxis("Horizontal");
+        if (Mathf.Abs(move) > 0f)
+        { 
+            latMovement.velocity = new Vector2(latMovement.velocity.x + move * speed, latMovement.velocity.y);
+        }
+
+        
+        
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {    
+            latMovement.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+
+    }
+}
